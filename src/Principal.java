2bfd1479efb1c5a14387d7c2ba0ea4d7f78c4de8
Nicolas Cunha <nico124cunha@gmail.com>
2025-01-11 16:57:53 -0300

    objeto meuFilme

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,9 @@
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
+        meuFilme.nome = "Suzume";
+        meuFilme.anoDeLancamento = 2022;
+        meuFilme.duracaoEmMinutos = 120;
 
     }
 }
