ad97270167bdb67a2a095f73e5d4dc4c0e4f339b
Nicolas Cunha <nico124cunha@gmail.com>
2025-01-11 16:57:32 -0300

    Esboço Filme

--- src/Filme.java
+++ src/Filme.java
@@ -1,4 +1,11 @@
 package PACKAGE_NAME;
 
 public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalAvaliacoes;
+    int duracaoEmMinutos;
+    
 }
