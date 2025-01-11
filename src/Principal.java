b5d1018af68a3b69d7eb877edc17bfd03fcf6487
Nicolas Cunha <nico124cunha@gmail.com>
2025-01-11 18:31:16 -0300

    Aula 2

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,17 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Suzume";
-        meuFilme.anoDeLancamento = 2022;
-        meuFilme.duracaoEmMinutos = 120;
+        meuFilme.setNome("Suzume");
+        meuFilme.setAnoDeLancamento(2022);
+        meuFilme.setDuracaoEmMinutos(120);
 
         meuFilme.exibirFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
